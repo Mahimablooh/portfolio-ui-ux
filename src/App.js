3666ecf14d7a5f1250922a4798649c3a7165b3ed
mahima.30420@gmail.com <mahima.30420@gmail.com>
2025-03-03 03:50:24 +0530

    corect routes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import ProtectedHabotAdminPortal from "./component/ProtectedProjectHabotAdminPor
 
 function App() {
   return (
-    <Router basename="/portfolio-ui-ux/">
+    <Router basename="portfolio-ui-ux">
       <div
         style={{
           display: "flex",
